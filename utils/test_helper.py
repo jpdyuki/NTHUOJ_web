80f001dae200c7474ceeede5a335e1416dc091aa
jpdyuki <joypad.y.t.h@gmail.com>
2015-11-19 00:07:05 +0800

    add function 'create_test_user' @param    username    account name of new test user @param    password    password of new test user @param    user_level    authority of new test user @return    a User object

--- utils/test_helper.py
+++ utils/test_helper.py
@@ -2,6 +2,8 @@ from django.test import Client
 
 import os
 
+from users.models import User
+
 
 TEST_PATH = ""
 
@@ -9,3 +11,9 @@ def create_test_directory(dir_name):
     TEST_PATH = dir_name
     if not os.path.isdir(TEST_PATH):
         os.makedirs(TEST_PATH)
+
+def create_test_user(username, password, user_level):
+    test_user = User.objects.create_user(username, password)
+    test_user.user_level = user_level
+    test_user.is_active = True
+    test_user.save()
