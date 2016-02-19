68fb5b971fe95c4f89a178fc06416be6c5365cb6
jpdyuki <joypad.y.t.h@gmail.com>
2016-02-20 03:08:29 +0800

    add some codes about testing the view 'status:view_code'

--- status/test_status.py
+++ status/test_status.py
@@ -310,3 +310,198 @@ class Tester_Status_rejudge(TestCase):
                             self.assertEqual(rejudged.status, Submission.WAIT)
                         else:
                             self.assertEqual(response.status_code, 403)
+
+
+class Tester_Status_view_code(TestCase):
+    """ test view 'status:view_code' """
+
+    def setUp(self):
+        create_test_directory()
+        preprocess(self)
+
+    def tearDown(self):
+        remove_test_directory()
+
+    def test_01_login(self):
+        """ test view 'view_code' """
+        # 1.user does not login
+        # Expectation: redirect to login page
+        sid = 1
+        target_url = reverse('status:view_code', args=[sid])
+        redirect_url = reverse('users:login') + '?next=' + target_url
+        response = self.ANONYMOUS_CLIENT.get(target_url)
+        self.assertRedirects(response, redirect_url)
+
+    def test_02_submission_not_found(self):
+        # 2.submission does not exist
+        # Expectation: error 404
+        sid = 1000000
+        target_url = reverse('status:view_code', args=[sid])
+        response = self.NORMAL_CLIENT.get(target_url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_03_file_not_found(self):
+        # 3.code file related to the submission does not exist
+        # Expectation: error 404
+        submission = create_submission(
+                        self.CONTEST_PROBLEMS[0], self.ADMIN_USER, self.STATUSES[0])
+        target_url = reverse('status:view_code', args=[submission.pk])
+        response = self.ADMIN_CLIENT.get(target_url)
+        filename = '%s.%s' % (submission.pk, get_extension(submission.language))
+        self.assertEqual(response.status_code, 404)
+
+    def test_04_permission_01(self):
+        # 4.admin can see everyone's detail
+        problem = self.CONTEST_PROBLEMS[0]
+        users = [self.ADMIN_USER, self.JUDGE_USER[0], self.JUDGE_USER[1],
+                 self.JUDGE_USER[3], self.JUDGE_USER[5], self.NORMAL_USER]
+        for i in range(len(self.STATUSES)):
+            submission = create_submission(
+                problem, users[i], self.STATUSES[i])
+            file_name, code = create_source_code(
+                CodeSubmitForm.SUBMIT_PATH, submission.pk,
+                get_extension(submission.language))
+            target_url = reverse('status:view_code', args=[submission.pk])
+            response = self.ADMIN_CLIENT.get(target_url)
+            remove_file_if_exists(file_name)
+            self.assertContains(response, code)
+
+    def test_05_permission_02(self):
+        # 5.no one except admin can see admin's detail
+        problem = self.CONTEST_PROBLEMS[0]
+        user = self.ADMIN_USER
+        for status in self.STATUSES:
+            submission = create_submission(problem, user, status)
+            target_url = reverse('status:view_code', args=[submission.pk])
+            response = self.JUDGE_CLIENT[0].get(target_url)
+            self.assertEqual(response.status_code, 403)
+            response = self.NORMAL_CLIENT.get(target_url)
+            self.assertEqual(response.status_code, 403)
+
+    def test_06_permission_03(self):
+        # 6.during the contest, only owner/coowner can view contestants' detail
+        users = self.JUDGE_USER + [self.NORMAL_USER]
+        clients = self.JUDGE_CLIENT + [self.NORMAL_CLIENT]
+        contest_submissions = []
+        for problem in self.CONTEST_PROBLEMS:
+            for i, user in enumerate(users):
+                for status in self.STATUSES:
+                    submission = create_submission(problem, user, status)
+                    contest_submissions.append(submission)
+                    for j, client in enumerate(clients):
+                        file_name, code = create_source_code(
+                            CodeSubmitForm.SUBMIT_PATH, submission.pk,
+                            get_extension(submission.language))
+                        target_url = reverse('status:view_code', args=[submission.pk])
+                        response = client.get(target_url)
+                        remove_file_if_exists(file_name)
+                        if j<3 and (i<3 or i>4):
+                            self.assertContains(response, code)
+                        else:
+                            self.assertEqual(response.status_code, 403)
+
+    #the following tests should under the condition that no contest is running
+
+    def test_07_permission_04(self):
+        # 7.a user can view his own detail in normal mode
+        users = self.JUDGE_USER + [self.NORMAL_USER]
+        clients = self.JUDGE_CLIENT + [self.NORMAL_CLIENT]
+        contest_submissions = all_submisions_when_contest_running(self)
+        stop_running_contest(self)
+        #submission during contest
+        for i, _ in enumerate(self.CONTEST_PROBLEMS):
+            for j, client in enumerate(clients):
+                for k, _ in enumerate(self.STATUSES):
+                    idx = i*42+j*6+k
+                    file_name, code = create_source_code(
+                        CodeSubmitForm.SUBMIT_PATH, contest_submissions[idx].pk,
+                        get_extension(contest_submissions[idx].language))
+                    target_url = reverse('status:view_code',
+                                         args=[contest_submissions[idx].pk])
+                    response = client.get(target_url)
+                    remove_file_if_exists(file_name)
+                    self.assertContains(response, code)
+        #submission after contest
+        for problem in self.CONTEST_PROBLEMS:
+            for i, user in enumerate(users):
+                for status in self.STATUSES:
+                    submission = create_submission(problem, user, status)
+                    file_name, code = create_source_code(
+                        CodeSubmitForm.SUBMIT_PATH, submission.pk,
+                        get_extension(submission.language))
+                    target_url = reverse('status:view_code', args=[submission.pk])
+                    response = clients[i].get(target_url)
+                    remove_file_if_exists(file_name)
+                    self.assertContains(response, code)
+
+    def test_08_permission_05(self):
+        # 8.a problem owner can view detail of his/her problem in normal mode
+        users = self.JUDGE_USER + [self.NORMAL_USER]
+        clients = self.JUDGE_CLIENT[3:5]
+        contest_submissions = all_submisions_when_contest_running(self)
+        stop_running_contest(self)
+        #submission during contest
+        for i, _ in enumerate(self.CONTEST_PROBLEMS):
+            for j, _ in enumerate(users):
+                for k, _ in enumerate(self.STATUSES):
+                    idx = i*42+j*6+k
+                    file_name, code = create_source_code(
+                        CodeSubmitForm.SUBMIT_PATH, contest_submissions[idx].pk,
+                        get_extension(contest_submissions[idx].language))
+                    target_url = reverse('status:view_code',
+                                         args=[contest_submissions[idx].pk])
+                    response = clients[i].get(target_url)
+                    response2 = clients[1-i].get(target_url)
+                    remove_file_if_exists(file_name)
+                    self.assertContains(response, code)
+                    if j!=4-i:
+                        self.assertEqual(response2.status_code, 403)
+        #submission after contest
+        for i, problem in enumerate(self.CONTEST_PROBLEMS):
+            for j, user in enumerate(users):
+                for status in self.STATUSES:
+                    submission = create_submission(problem, user, status)
+                    file_name, code = create_source_code(
+                        CodeSubmitForm.SUBMIT_PATH, submission.pk,
+                        get_extension(submission.language))
+                    target_url = reverse('status:view_code', args=[submission.pk])
+                    response = clients[i].get(target_url)
+                    response2 = clients[1-i].get(target_url)
+                    remove_file_if_exists(file_name)
+                    self.assertContains(response, code)
+                    if j!=4-i:
+                        self.assertEqual(response2.status_code, 403)
+
+    def test_09_permission_06(self):
+        # 9.in normal mode, contest owner/coowner can still view detail during the contest
+        users = self.JUDGE_USER + [self.NORMAL_USER]
+        clients = self.JUDGE_CLIENT[0:3]
+        contest_submissions = all_submisions_when_contest_running(self)
+        stop_running_contest(self)
+        #submission during contest (permission is the same as the moment contest was running)
+        for i, _ in enumerate(self.CONTEST_PROBLEMS):
+            for j, _ in enumerate(users):
+                for k, _ in enumerate(self.STATUSES):
+                    idx = i*42+j*6+k
+                    target_url = reverse('status:view_code',
+                                         args=[contest_submissions[idx].pk])
+                    for client in clients:
+                        file_name, code = create_source_code(
+                            CodeSubmitForm.SUBMIT_PATH, contest_submissions[idx].pk,
+                            get_extension(contest_submissions[idx].language))
+                        response = client.get(target_url)
+                        remove_file_if_exists(file_name)
+                        if j<3 or j>4:
+                            self.assertContains(response, code)
+                        else:
+                            self.assertEqual(response.status_code, 403)
+        # submission after contest (no permission to view detail)
+        for problem in self.CONTEST_PROBLEMS:
+            for j, user in enumerate(users):
+                for status in self.STATUSES:
+                    submission = create_submission(problem, user, status)
+                    target_url = reverse('status:view_code', args=[submission.pk])
+                    for p, client in enumerate(clients):
+                        if p!=j:
+                            response = client.get(target_url)
+                            self.assertEqual(response.status_code, 403)
